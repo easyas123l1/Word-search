80cab1b32aed72502c841eb1c05a6cfe1c1c83e6
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-03-11 12:26:45 -0500

    First commit Crossword Puzzle Generator, Removed default React

---
+++ crossword/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            CROSSWORD SEARCH!!!
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
