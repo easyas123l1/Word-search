3b63ea23c77d66308db99c56839416e280d2b772
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-03-11 17:53:40 -0500

    added css to Navigation bar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 
+import './Navigation.css';
+
 class Navigation extends Component {
   render() {
     return (
-      <div className='NavBar'>
-        <header>
-          <p>Home</p>
-          <p>Browse Puzzles</p>
-          <p>About Me</p>  
-        </header>          
-      </div>
+      <header className='navbar'>
+        <nav className ='navbar_navigation'>
+          <div></div>
+          <div className="navbar_logo"><a href="/">WORD SEARCH</a></div>
+          <div className="Spacer" />
+          <div className="navbar_navigation-items">
+            <ul>
+              <li><a href='/'>Home</a></li>              
+              <li><a href='/'>Browse Puzzles</a></li>   
+              <li><a href='/'>About Me</a></li>   
+            </ul>
+          </div>
+        </nav> 
+      </header>          
     )
   }
 }
