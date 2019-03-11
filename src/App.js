85076c0c7a01e85f0466ac623af59a8e0c9453d1
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-03-11 17:22:40 -0500

    added navigation component which contains navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './Components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Wordsearch
-          </p>
-        </header>
+        <Navigation />
       </div>
     );
   }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className='NavBar'>
+        <header>
+          <p>Home</p>
+          <p>Browse Puzzles</p>
+          <p>About Me</p>  
+        </header>          
+      </div>
+    )
+  }
+}
+
+export default Navigation;
