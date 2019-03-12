7a385addda20b9fd1bfe6f3d36bbaab5425a8c54
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-03-12 09:13:01 -0500

    added WordEntry and better file organization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './Components/Navigation';
+import Navigation from './Components/Navigation/Navigation';
+import WordEntry from './Components/Wordentry/WordEntry';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <WordEntry />
       </div>
     );
   }
---
+++ src/Components/Wordentry/WordEntry.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './WordEntry.css';
+
+class WordEntry extends Component {
+  render() {
+    return (
+      <div>
+        <input 
+        word=''
+        type='text' />
+      </div>
+    );
+  }
+}
+
+export default WordEntry;
