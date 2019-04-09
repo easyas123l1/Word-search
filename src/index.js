5a5960e5448109dbb21fd324cb268929071ea075
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-04-09 16:05:28 -0500

    heroku rdy

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-App.set( 'port', ( process.env.PORT || 5000 ));
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
